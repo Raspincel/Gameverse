0c260c7ed6233bc0a7d9ad25be513451c96e1f80
Raspincel <raspincel@gmail.com>
2023-06-25 23:04:12 -0300

    fix?

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
-export const GlobalStyle = createGlobalStyle`
+export const GlobalStyle = createGlobalStyle<{}>`
     :root {
         --ultra-dark: #333;
         --ultra-light: #EFEFEF;
