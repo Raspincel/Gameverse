dc0fe973dd529f828a3ecfdb619323a0e272c765
Raspincel <raspincel@gmail.com>
2023-08-10 05:42:46 -0300

    .

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -18,20 +18,6 @@ export default function Footer() {
                         </a>
                     </span>
 
-                    <span className="icon-section">
-                        <a href="https://www.linkedin.com/in/ian-silva-09b410237/" target="_blank">
-                            <FaLinkedin/>
-                        </a>
-                        <a href="https://www.linkedin.com/in/ian-silva-09b410237/" target="_blank">
-                            <p>Linkedin</p>
-                        </a>
-                    </span>
-
-                </div>
-                <div className="project-infos">
-                    <p>
-                        Este website foi desenvolvido com o propósito de concorrer ao processo de seleção de uma vaga para Estágio Frontend com React pela App Masters, não possuindo quaisquer fins lucrativos ou de comercialização. 
-                    </p>
                 </div>
             </FooterContainer>
         </IconContext.Provider>
